f233a369d80ab743e2944c4cbb8456d03e1b1a5f
Jordan Heise <jordanheise@Jordans-iMac.local>
2020-09-29 16:14:16 -0400

    started PyPoll Code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,4 @@
-import csv
-
-with open('/Users/jordanheise/Desktop/python-challenge/PyBank/Resources/budget_data.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        print(row)
-        
+import pandas        
 import datetime
 
 end_date = datetime.datetime(2017,2,1)
@@ -13,6 +7,13 @@ start_date = datetime.datetime(2010, 1, 1)
 num_months = (end_date.year - start_date.year) * 12 + (end_date.month - start_date.month)
 
 
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
 print("Financial Analysis")
 print("--------------------------")
 print("Total Months:", num_months)
+
+
+budgetData = pandas.read_csv('/Users/jordanheise/Desktop/python-challenge/PyBank/Resources/budget_data.csv', delimiter=',')
+
+sum_PL = budgetData['Profit/Losses'].sum()
