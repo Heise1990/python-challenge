18ddb96b828c9f32874e1cfa7311d840e05c067e
Jordan Heise <jordanheise@Jordans-iMac.local>
2020-10-01 15:51:18 -0400

    updates to PyPoll Code

--- PyPoll/Analysis/PyPoll Code.py
+++ PyPoll/Analysis/PyPoll Code.py
@@ -1,29 +1,35 @@
 import csv
 
-def candidates(csvfile):
-    candidate = str(csvfile[2])
-    county = str(csvfile[1])
-    voterID = int(csvfile[0])
-    
-
+candidates = []
 votesCast = 0
 voterCount = 0
+pollerData = ['1', '2']
+    
 
 with open ('/Users/jordanheise/Desktop/nu-chi-data-pt-09-2020-u-c/03-Python/HW/Instructions/PyPoll/Resources/election_data.csv') as csvfile:
      csvreader = csv.reader(csvfile, delimiter=",")
      header_row = next(csvfile)
-     print(f"Header: {header_row}")
      
      
-
-
-    
-     # need to sum total # of voters
+     # need to total up # of votes per candidate and divide by Total Votes
+     # to determine percentage
      
      voters = [0]
      
      for row in csvreader:
          voterCount = voterCount + 1
+         candidate = [2]
+         
+     # comb through data set to count instances of candidate votes earned
+         
+         if candidate in candidates:
+             candidate_index = candidates.index(candidate)
+             voterCount[candidate_index] = voterCount[candidate_index] + 1
+             
+         else:
+             candidates.append(candidate)
+             voterCount.append(1)
+             
          
      
      print("Election Results")
