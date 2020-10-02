0a24b0e4dd01685bcb43761f2f887517bdae81be
Jordan Heise <jordanheise@Jordans-iMac.local>
2020-10-02 17:05:03 -0400

    added final code

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,7 +10,7 @@ PLchange = 0
 with open ('/Users/jordanheise/Desktop/python-challenge/PyBank/Resources/budget_data.csv') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header_row = next(csvfile)
-        
+ 
  # need to total up # of months in data series
  
     months = [0]
@@ -31,3 +31,30 @@ with open ('/Users/jordanheise/Desktop/python-challenge/PyBank/Resources/budget_
             previousPL = currentPL
             
     sumPL = sum(PLchanges)
+    avgPL = round(sumPL/(numberMonths - 1),2)
+    
+    grtstChange = max(PLchanges)
+    smlstChange = min(PLchanges)
+    
+    grtstIncrs = PLchanges.index(grtstChange)
+    grtstDcrs = PLchanges.index(smlstChange)
+    
+    bestMonthPL = PLchanges.index(grtstChange)
+    worstMonthPL = PLchanges.index(smlstChange)
+    
+    bestMonth = months[bestMonthPL]
+    worstMonth = months[worstMonthPL]
+    
+    output = (
+	f"Financial Analysis\n"
+	f"-------------------------\n"
+	f"Total Months: {numberMonths}\n"
+	f"Total: ${netPL}\n"
+	f"Average Change: ${avgPL}\n"
+	f"Greatest Increase in Profits: {bestMonth} (${grtstChange})\n"
+	f"Greatest Decrease in Profits: {worstMonth} (${smlstChange})\n")
+
+print(output)
+
+with open("PYBankFinancials.txt", "w") as txt_file:
+	txt_file.write(output)
